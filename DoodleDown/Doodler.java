cda7f727ccff5d40e43c9f664187573516e11d20
Yang Song <ysong819@gmail.com>
2014-02-20 20:17:28 -0800

    Add movement, and fall

--- DoodleDown/Doodler.java
+++ DoodleDown/Doodler.java
@@ -8,12 +8,55 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Doodler extends Actor
 {
+    private int speed = 8;
+    private int vSpeed = 0;
+    private int acceleration = 1;
+    
     /**
      * Act - do whatever the Doodler wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+       checkKeys();
+       checkFall();
     }    
+    
+    private void checkKeys() {
+        if(Greenfoot.isKeyDown("left")) {
+            setImage("doodler.png");
+            moveLeft();
+        }
+        if(Greenfoot.isKeyDown("right")) {
+            setImage("doodler.png");
+            moveRight();
+        }
+    }
+    
+    public boolean onGround() {
+        Actor under = getOneObjectAtOffset(0, 20, Ground.class);
+        return under != null;
+    }
+    
+    public void checkFall() {
+        if(onGround()) {
+            vSpeed = 0;
+        }
+        else {
+            fall();
+        }
+    }
+    
+    public void fall() {
+        setLocation(getX(), getY() + vSpeed);
+        vSpeed = vSpeed + acceleration;
+    }
+    
+    public void moveLeft() {
+        setLocation(getX() - speed, getY());
+    }
+    
+    public void moveRight() {
+        setLocation(getX() + speed, getY());
+    }
 }
