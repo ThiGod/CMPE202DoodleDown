7288f1ff82f257d8dc749cade6f91cee92913286
lingzhang-lyon <zhangling.ice@gmail.com>
2014-02-20 21:56:13 -0800

    add initial screen, dynamically add ground

--- DoodleDown/DoodleWorld.java
+++ DoodleDown/DoodleWorld.java
@@ -9,6 +9,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class DoodleWorld extends World
 {
 
+    private int timer = 0;  
+    private static int CHECKPOINT = 80; 
+    
     /**
      * Constructor for objects of class DoodleWorld.
      * 
@@ -17,5 +20,45 @@ public class DoodleWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
     }
+
+    /**
+     * Prepare the world for the start of the program. That is: create the initial
+     * objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Doodler doodler = new Doodler();
+        addObject(doodler, 175, 34);
+        Ground ground = new Ground();
+        addObject(ground, 162, 129);
+        Ground ground2 = new Ground();
+        addObject(ground2, 376, 238);
+        Ground ground3 = new Ground();
+        addObject(ground3, 183, 338);
+    }
+    
+ 
+    public void act()
+    {  
+     if (timer != CHECKPOINT) 
+        timer++;
+     else 
+      {
+        timer=0;
+        addGround();
+      }
+    }
+    
+  
+    
+    public void addGround()
+    {
+      
+        addObject(new Ground(), Greenfoot.getRandomNumber(600), 400);
+       
+    }
+    
 }
+
